b972ed7b737c3221be17397006843075f04d12b3
meliodas137 <mayankgt1043@gmail.com>
2024-04-23 20:33:05 -0400

    using spring boot application annotation

--- Frontend/src/main/java/org/proj/Main.java
+++ Frontend/src/main/java/org/proj/Main.java
@@ -1,5 +1,9 @@
 package org.proj;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
--- src/main/java/org/proj/Main.java
+++
@@ -1,7 +0,0 @@
-package org.proj;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
