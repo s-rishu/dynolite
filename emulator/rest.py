9efe60eb29f4e38eb2a3f0c472a774eb71d49b76
meliodas137 <mayankgt1043@gmail.com>
2024-04-26 19:06:29 -0400

    using nodes to send messages instead of nodeIds

--- emulator/rest.py
+++ emulator/rest.py
@@ -4,16 +4,10 @@ from libs import ProcessNode
 logger = logging.getLogger("dynolite")
 
 class REST:
-    idNodeMap = {}
-
     def __init__(self):
         logger.info("Creating a REST util to simulate message passing")
 
     @staticmethod
-    def send(_from, to, msg):
-        node: ProcessNode = REST.idNodeMap[to]
-        node.rcv(_from, msg)
-    
-    @staticmethod
-    def addNode(id, node: ProcessNode):
-        REST.idNodeMap[id] = node
+    def send(_from, to: ProcessNode, msg):
+        to.rcv(_from, msg)
+
