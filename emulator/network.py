acb85b0f74502b83341e02d4aaab1e3d33ae6f61
s-rishu <rishu.singh1203@gmail.com>
2024-04-27 16:52:27 -0400

    added network emulator interface

--- emulator/network.py
+++ emulator/network.py
@@ -5,9 +5,44 @@ logger = logging.getLogger("dynolite")
 
 class Network:
     def __init__(self):
-        logger.info("Creating a network util to simulate message passing")
+        logger.info("Creating a network..")
 
     @staticmethod
     def send(_from, to: ProcessNode, msg):
+        #TODO: remove
         to.rcv(_from, msg)
+
+    @classmethod
+    def make_unreachable(cls, from_nodes, to_nodes):
+        """cuts connection from all from nodes to all to nodes"""
+        pass
+
+    @classmethod
+    def is_reachable(cls, from_node, to_node):
+        """returns true of a connection exists between from and to node"""
+        pass
+
+    @classmethod
+    def reset(cls):
+        """resets entire network"""
+        pass
+
+    @classmethod
+    def send_message(cls, msg):
+        """send message"""
+        pass
+
+    @classmethod
+    def cancel_timers_to(cls, dest):
+        """cancel all timers waiting for a response from dest node"""
+        pass
+
+    @classmethod
+    def run_tasks(cls):
+        """run all tasks in the queue"""
+        pass
+
+
+
+
 
