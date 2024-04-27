4f96d4bac7c2a695d71c48d3a1acd0ed7d1493fe
s-rishu <rishu.singh1203@gmail.com>
2024-04-27 17:11:41 -0400

    added test files

---
+++ tests/test_logic.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestLogic(unittest.TestCase):
+    """Test correctness of Dynamo logic"""
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_performance.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestPerformance(unittest.TestCase):
+    """Test performance of Dynamo"""
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
